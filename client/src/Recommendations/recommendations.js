405aa9f2d906f77749177516fd1a11705feda9cc
beneweiler1 <beneweiler1@gmail.com>
2023-04-13 12:44:36 -0600

    added stream graph

--- client/src/Recommendations/recommendations.js
+++ client/src/Recommendations/recommendations.js
@@ -1,7 +1,12 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import Data from '../StreamGraph/sampleData.json'
+import { StreamGraph } from '../StreamGraph/StreamGraph'
 
 export default function Recommendations() {
     return (
-        <p>recommendations</p>
+        <div className="home_page">
+            <p>recommendations</p>
+            <StreamGraph data={Data} />
+        </div>
     )
 }
