20b7f08ccdef633950289f3d69d9dfb4b49d5961
beneweiler1 <beneweiler1@gmail.com>
2023-04-23 14:26:39 -0600

    hover for metric info

--- client/src/sliderForm/EditMetricForm.js
+++ client/src/sliderForm/EditMetricForm.js
@@ -1,8 +1,5 @@
 import { useState } from 'react';
-import { Slider, Typography, Box, Grid, Button } from '@mui/material';
-import { red } from '@mui/material/colors';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-
+import { Slider, Typography, Box, Grid, Button, Tooltip } from '@mui/material';
 
 
 export function EditMetricForm() {
@@ -15,7 +12,7 @@ export function EditMetricForm() {
   const [instrumentalness, setInstrumentalness] = useState(0);
   const [acousticness, setAcousticness] = useState(0)
   const [speechiness, setSpeechiness] = useState(0)
-  
+
 
   const handleLivenessChange = (event, newValue) => {
     setLiveness(newValue);
@@ -64,9 +61,11 @@ export function EditMetricForm() {
     <Box>
       <Grid container spacing={3} alignItems="center">
         <Grid item xs={12} sm={6}>
-          <Typography id="input-slider" gutterBottom>
-            Liveness: {liveness}
-          </Typography>
+          <Tooltip title="presence of an audience in the recording" arrow>
+            <Typography id="input-slider" gutterBottom>
+              Liveness: {liveness}
+            </Typography>
+          </Tooltip>
           <Slider
             value={liveness}
             onChange={handleLivenessChange}
@@ -78,9 +77,11 @@ export function EditMetricForm() {
           />
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Typography id="input-slider" gutterBottom>
-            Energy: {energy}
-          </Typography>
+          <Tooltip title="represents a perceptual measure of intensity and activity" arrow>
+            <Typography id="input-slider" gutterBottom>
+              Energy: {energy}
+            </Typography>
+          </Tooltip>
           <Slider
             value={energy}
             onChange={handleEnergyChange}
@@ -92,9 +93,11 @@ export function EditMetricForm() {
           />
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Typography id="input-slider" gutterBottom>
-            Danceability: {danceability}
-          </Typography>
+          <Tooltip title="how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity" arrow>
+            <Typography id="input-slider" gutterBottom>
+              Danceability: {danceability}
+            </Typography>
+          </Tooltip>
           <Slider
             value={danceability}
             onChange={handleDanceabilityChange}
@@ -106,9 +109,11 @@ export function EditMetricForm() {
           />
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Typography id="input-slider" gutterBottom>
-            Valence: {valence}
-          </Typography>
+          <Tooltip title="musical positiveness conveyed by a track" arrow>
+            <Typography id="input-slider" gutterBottom>
+              Valence: {valence}
+            </Typography>
+          </Tooltip>
           <Slider
             value={valence}
             onChange={handleValenceChange}
@@ -120,9 +125,11 @@ export function EditMetricForm() {
           />
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Typography id="input-slider" gutterBottom>
-            Instrumentalness: {instrumentalness}
-          </Typography>
+          <Tooltip title="predicts whether a track contains no vocals" arrow>
+            <Typography id="input-slider" gutterBottom>
+              Instrumentalness: {instrumentalness}
+            </Typography>
+          </Tooltip>
           <Slider
             value={instrumentalness}
             onChange={handleInstrumentalnessChange}
@@ -134,9 +141,11 @@ export function EditMetricForm() {
           />
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Typography id="input-slider" gutterBottom>
-            Speechiness: {speechiness}
-          </Typography>
+          <Tooltip title="presence of spoken words in a track" arrow>
+            <Typography id="input-slider" gutterBottom>
+              Speechiness: {speechiness}
+            </Typography>
+          </Tooltip>
           <Slider
             value={speechiness}
             onChange={handleSpeechinessChange}
@@ -148,9 +157,11 @@ export function EditMetricForm() {
           />
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Typography id="input-slider" gutterBottom>
-            Acousticness: {acousticness}
-          </Typography>
+          <Tooltip title="presence of an acoustic instrument and voices" arrow>
+            <Typography id="input-slider" gutterBottom>
+              Acousticness: {acousticness}
+            </Typography>
+          </Tooltip>
           <Slider
             value={acousticness}
             onChange={handleAcousticnessChange}
@@ -162,10 +173,10 @@ export function EditMetricForm() {
           />
         </Grid>
         <Grid item xs={12} sm={6}>
-            <Button variant="outlined" sx={{ width: '100%', maxWidth: 200 }}
-              onClick={handleButtonClick}>
-              Re Roll
-            </Button>
+          <Button variant="outlined" sx={{ width: '100%', maxWidth: 200 }}
+            onClick={handleButtonClick}>
+            Re Roll
+          </Button>
         </Grid>
       </Grid>
     </Box>
