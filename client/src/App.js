4f0e689782d7d1a446555286ce1812e534bd6f82
beneweiler1 <beneweiler1@gmail.com>
2023-04-23 13:47:32 -0600

    reccommendations sliders

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route, BrowserRouter} from "react-router-dom";
 import React from "react";
 import Home from "./Home/home"
 import Recommendations from "./Recommendations/recommendations"
+import { EditMetricForm } from './sliderForm/EditMetricForm';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/recommendations" element={<Recommendations/>}/>
+          <Route path="/form" element={<EditMetricForm/>}/> 
         </Routes>
      </BrowserRouter>
     </div>
--- client/src/Recommendations/recommendations.js
+++ client/src/Recommendations/recommendations.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from "react";
 import Data from '../StreamGraph/sampleData.json'
 import { StreamGraph } from '../StreamGraph/StreamGraph'
+import './recommendations.css'
+import { EditMetricForm } from "../sliderForm/EditMetricForm";
 
 export default function Recommendations() {
     return (
-        <div className="home_page">
-            <p>recommendations</p>
-            <StreamGraph data={Data} />
-        </div>
+<div className="home_page">
+  <p>recommendations</p>
+  <div className="container">
+    <div className="metricForm">First Div</div>
+    <div className="secondDiv">
+      <div className="metricForm editForm">
+        <EditMetricForm data={Data}/>
+      </div>
+    </div>
+  </div>
+  <StreamGraph data={Data} />
+</div>
     )
 }
