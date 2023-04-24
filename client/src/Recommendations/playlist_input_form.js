16b559363558c14e076c7752b13d1df3e3c0fc1f
maha4208 <mastha_64@yahoo.com>
2023-04-24 10:54:16 -0600

    working fetch with state set via callback

--- client/src/Recommendations/playlist_input_form.js
+++ client/src/Recommendations/playlist_input_form.js
@@ -1,18 +1,71 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import "./recommendations.css"
+const url = require('url-parse');
 
 
 
-export default function PlaylistInputForm () {
+
+export default function PlaylistInputForm ({
+    songs,
+    artists,
+    ids,
+    list_id,
+    onSongsUpdate,
+    onArtistsUpdate,
+    onIdsUpdate,
+    onListIdUpdate,
+}) {
     
     const [playlist_id, setPlaylist_id] = useState('');
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     function handlePlaylistChange(event) {
         setPlaylist_id(event.target.value);
     }
 
     function handleSubmit(event) { 
-    
+        
+        event.preventDefault()
+        setIsLoading(true)
+        
+        // this formulates our request
+        const spotifyUrl = new url(playlist_id)
+        console.log(spotifyUrl);
+        const path = spotifyUrl.pathname; 
+        const id = path.split('/').pop();
+        console.log(id)
+        const request = "http://127.0.0.1:8080/playlist/"+id
+        console.log(request)
+
+
+        const options = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            }
+        };
+        //making the request
+        fetch(request, options)
+            .then (response => {
+                if(!response.ok) {
+                    throw new Error("fetch failed")
+                }
+                else{
+                    return response.json()
+                }
+            })
+            .then (data => {
+                console.log(data)
+                onSongsUpdate(data.new_playlist.name)
+                onArtistsUpdate(data.new_playlist.artist)
+                onIdsUpdate(data.new_playlist.id)
+                onListIdUpdate(data.user_playlist_id)
+            })
+            .catch (error => {
+                console.error(error)
+            })
     }
 
     return ( 
--- client/src/Recommendations/recommendations.js
+++ client/src/Recommendations/recommendations.js
@@ -9,7 +9,32 @@ export default function Recommendations() {
     const location = useLocation();
     const params = new URLSearchParams(location.search);
     const authorizationCode = params.get('authorizationCode');
-    console.log("REC: "+authorizationCode)
+
+    const [song_names, set_song_names] = useState([])
+    const [artist_names, set_artist_names] = useState([])
+    const [playlist_id, set_playlist_id] = useState('')
+    const [track_ids, set_track_ids] = useState([])
+
+    const handleSongsUpdate = (newData) => {
+        set_song_names(newData)
+        console.log(song_names)
+    }
+
+    const handleArtistsUpdate = (newData) => {
+        set_artist_names(newData)
+        console.log(artist_names)
+    }
+
+    const handleIdsUpdate = (newData) => {
+        set_track_ids(newData)
+        console.log(track_ids)
+    }
+
+    const handleListIdUpdate = (newData) => {
+        set_playlist_id(newData)
+        console.log(playlist_id)
+    }
+
 
     return (
         <div className="rec_header_and_input">
@@ -17,7 +42,16 @@ export default function Recommendations() {
                 <img src={tastemaker_logo} className="logo_2" alt="TASTEMAKER" />
             </div>
             <div className="playlist_input">
-                <PlaylistInputForm />
+                <PlaylistInputForm 
+                    songs={song_names} 
+                    artists={artist_names} 
+                    ids={track_ids} 
+                    list_id={playlist_id} 
+                    onSongsUpdate={handleSongsUpdate}
+                    onArtistsUpdate={handleArtistsUpdate}
+                    onIdsUpdate={handleIdsUpdate}
+                    onListIdUpdate={handleListIdUpdate}
+                />
             </div>
         </div>
     )
